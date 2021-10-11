ca710b9ae1bb2c391fcded5bb79cab14d12c6123
Canadianfaller7 <canadianfaller7@gmail.com>
2021-10-10 19:31:02 -0600

    Updated files

---
+++ Basic JavaScript Porjects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,44 @@
+/* This is a way you can write a text*/
+document.write('Hello World! ');
+
+/* the br is a break just like in html
+
+let is also the way to make a changable variable or you can
+
+use const for a constant variable(non changable variable)*/
+let A = "This is a string \<br>";
+
+document.write(A);
+
+/* By adding the \ it allows up to add a ' to the text like (it's) */
+document.write("\<br>Let\'s go outside and skateboard if the weather is good"
+
++ ", but if the weather isn\'t good then we can just stay inside.\<br>");
+
+let B = "This is a variable holding a string value";
+
+//This will set up an alert message when you open up the page
+window.alert(B);
+
+let C = "\<br>Here I am combinging this text" +
+
+" with this text using a \"+\" sign\<br>\<br>";
+
+document.write(C);
+
+//This is multiple vars in one line of code and you can call any 
+//of them in the document.write() 
+let D = "Weather types:", S = "Summer",
+
+F = "Fall", SP = "Spring", W = "Winter";
+
+document.write(S);
+
+var purple = "\<br>\<br>This will be written in purple\<br>\<br>";
+
+var purple = purple.fontcolor("purple");
+
+document.write(purple);
+
+// This is a staement(document.write) that contains an expression(4 + 3)
+document.write(4 + 3)
