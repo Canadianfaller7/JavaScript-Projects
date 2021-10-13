17a62543dceb3a0226fa3c941c72da5f8fc22a69
Canadianfaller7 <canadianfaller7@gmail.com>
2021-10-13 02:27:19 -0600

    Added new project

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,18 @@
+
+/*This is a dictionary and we are calling at the bottom to show the breed
+of the Species we chose to use in a text in html when you 
+click on the click me */
+function my_Dictionary() {
+    let Animal = {
+        Species: "Dog",
+        Color: "Black",
+        Breed: "Goldendoodle",
+        Age: 2,
+        Sound: "Bark"
+    };
+
+document.getElementById("Dictionary").innerHTML = Animal.Breed;
+}
+
+// here we are deleting the color from our dictionary
+delete Animal.Colorl;
